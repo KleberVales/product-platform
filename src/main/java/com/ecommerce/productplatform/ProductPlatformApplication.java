b858a3ee62cb95aebe97f6aa7a74444936c9cfea
Kleber Vales <klebervales.dev@gmail.com>
2026-02-05 17:20:58 -0300

    Initial commit

---
+++ src/main/java/com/ecommerce/productplatform/ProductPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.productplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ecommerce/productplatform/ProductPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.productplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
