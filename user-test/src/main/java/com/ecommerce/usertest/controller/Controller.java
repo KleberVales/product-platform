7ffb78a124024042092e72d217c6c4eb656077c4
Kleber Vales <klebervales.dev@gmail.com>
2026-02-06 22:51:11 -0300

    implementing test

---
+++ user-test/src/main/java/com/ecommerce/usertest/controller/Controller.java
@@ -0,0 +1,31 @@
+package com.ecommerce.usertest.controller;
+
+import com.ecommerce.usertest.dto.LoginRequestDTO;
+import com.ecommerce.usertest.dto.LoginResponseDTO;
+import com.ecommerce.usertest.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class Controller {
+
+    private UserService service;
+
+    public Controller(UserService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/login")
+    public LoginResponseDTO login(@RequestBody LoginRequestDTO dto) {
+
+        service.findByEmail(dto);
+        LoginResponseDTO loginResponseDTO = new LoginResponseDTO();
+
+        loginResponseDTO.setPasswordHash(dto.getPasswordHash());
+
+        return loginResponseDTO;
+    }
+}
--- user-test/src/main/java/com/ecommerce/usertest/dto/LoginRequestDTO.java
+++ user-test/src/main/java/com/ecommerce/usertest/dto/LoginRequestDTO.java
@@ -5,11 +5,22 @@ public class LoginRequestDTO {
 
     private String email;
 
+    private String passwordHash;
+
     public String getEmail() {
         return email;
     }
 
     public void setEmail(String email) {
+
         this.email = email;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
     }
 }
